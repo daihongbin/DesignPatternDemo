ea49ca0116eda338615f3f74a954ea9b43337725
daihb <daihb@DESKTOP-R31MQAB>
2019-08-06 17:30:13 +0800

    贫血均衡模式。

---
+++ AnotherDesignPattern/SingletonSample/LoadBalancer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SingletonSample
+{
+    /// <summary>
+    /// 假装自己是一个负载均衡器
+    /// </summary>
+    public class LoadBalancer
+    {
+        //私有静态变量，存储唯一实例
+        private static LoadBalancer instance = null;
+
+        private IList<CustomServer> serverList = null;
+
+        //私有构造函数
+        private LoadBalancer()
+        {
+            serverList = new List<CustomServer>();
+        }
+
+        //公共静态方法，返回唯一实例
+        public static LoadBalancer GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new LoadBalancer();
+            }
+
+            return instance;
+        }
+
+        public void AddServer(CustomServer server)
+        {
+            serverList.Add(server);
+        }
+
+        public void RemoveServer(string serverName)
+        {
+            var server = serverList.FirstOrDefault(r => r.Name == serverName);
+            serverList.Remove(server);
+        }
+
+        private Random rand = new Random();
+        public CustomServer GetServer()
+        {
+            int index = rand.Next(serverList.Count);
+            return serverList[index];
+        }
+    }
+
+    public class CustomServer
+    {
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+    }
+}
---
+++ AnotherDesignPattern/SingletonSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SingletonSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadBalancer balancer, balancer2, balancer3;
+            balancer = LoadBalancer.GetInstance();
+            balancer2 = LoadBalancer.GetInstance();
+            balancer3 = LoadBalancer.GetInstance();
+
+            //判断负载均衡器是否相同
+            if (balancer == balancer2 && balancer == balancer3 && balancer2 == balancer3)
+            {
+                Console.WriteLine("^_^：服务负载均衡器是唯一的");
+            }
+
+            //增加服务器
+            balancer.AddServer(new CustomServer {Name = "Server1" });
+            balancer.AddServer(new CustomServer {Name = "Server2" });
+            balancer.AddServer(new CustomServer {Name = "Server3" });
+            balancer.AddServer(new CustomServer {Name = "Server4" });
+
+            //模拟客户端请求的分发
+            for (int i = 0;i < 10;i++)
+            {
+                CustomServer server = balancer.GetServer();
+                Console.WriteLine("该请求已分配至：" + server.Name);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
